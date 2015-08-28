147e60bff28a180cef07ed1c4021f14817cb7da3
shreyasj2006 <shreyasj2006@gmail.com>
2015-08-28 00:18:06 -0700

    Initial commit
    Initial version

---
+++ app.js
@@ -0,0 +1,17 @@
+/**
+ * Created by shreyas on 8/27/15.
+ */
+
+(function() {
+
+    'use strict';
+
+    angular.module('uxApp',[]);
+
+    angular.module('uxApp').controller('MainController',MainController);
+
+    function MainController() {
+
+    }
+
+})();
